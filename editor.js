b85d69d75c0c6dcd2e68b1d9a47e16b1276fa57f
Duncan <duncansreeves@gmail.com>
2016-07-28 15:38:26 +0000

    More style

---
+++ editor.js
@@ -0,0 +1,13 @@
+// JavaScript File
+
+function runHtml() {
+    $("#htmlHolder").html($("#htmlText").val());
+}
+
+function runCss() {
+    $().html($("#cssText").val());
+}
+
+function runJavascript() {
+    $().html($("#jsText").val());
+}
