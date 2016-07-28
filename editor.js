c00e9478bf4da129033c3ed606b3580072eeecc9
Duncan <duncansreeves@gmail.com>
2016-07-28 18:05:18 +0000

    Implemented links for the html textbox and tried to for the css and javascript

--- editor.js
+++ editor.js
@@ -1,13 +1,38 @@
 // JavaScript File
 
-function runHtml() {
-    $("#htmlHolder").html($("#htmlText").val());
+// GLOBAL VARIABLE DECLARATOINS
+var iframeBody;
+var iframeHead;
+var iframeScript;
+var iframeCss;
+
+
+$(document).ready(function() { 
+    $(function() {
+    	var $frame = $('<iframe style="width:99%; height:100%;" >');
+    	$("#htmlHolder").html( $frame );
+    	setTimeout( function() {
+    		var doc = $frame[0].contentWindow.document;
+    		iframeBody = $('body',doc);
+    		iframeHead = $('head',doc);
+    		iframeScript = $('<script type="text/javascript"></script>');
+    		iframeCss= $('<link rel="stylesheet" type="text/css" />');
+    		iframeHead.html(iframeScript[0] + iframeCss[0]);
+    	}, 1);
+    });
+});
+
+function runHtml()
+{
+    iframeBody.html($("#htmlText").val());
 }
 
-function runCss() {
-    $().html($("#cssText").val());
+function runJavascript()
+{
+    iframeScript.html($("#jsText").val());
 }
 
-function runJavascript() {
-    $().html($("#jsText").val());
+function runCss()
+{
+    iframeCss.html($("#cssText").val());
 }
